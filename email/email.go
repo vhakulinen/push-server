4b05648ac28149cd900f1bc2ffa91b1099f8c6bd
Ville Hakulinen <ville.hakulinen@gmail.com>
2015-03-19 20:40:14 +0200

    Sendgrid support

--- email/email.go
+++ email/email.go
@@ -5,50 +5,85 @@ import (
 	"log"
 	"net/smtp"
 
+	"github.com/sendgrid/sendgrid-go"
 	"github.com/vhakulinen/push-server/config"
 	"github.com/vhakulinen/push-server/db"
 )
 
 var (
-	addr     string
+	addr   string
+	host   string
+	from   string
+	domain string
+
 	username string
 	password string
-	host     string
-	from     string
-	domain   string
 
 	configLoaded = false
 )
 
 const regMessageRaw = "This email address was used while registering to push-serv\n" +
-	"To complite this regitration process, follow this link: %s\n\n" +
+	"To complite this regitration process, follow this link:\n\nhttps://%s\n\n" +
 	"If you did not register to this service, ignore this message\n\nDo not reply to this message"
 
-var SendRegistrationEmail = func(u *db.User) error {
-	if !configLoaded {
-		loadConfig()
-	}
+var sendMail func(m, email string) error
+
+var sendSMTP = func(m, email string) error {
 	auth := smtp.PlainAuth("", username, password, host)
-	uri := fmt.Sprintf("%s/activate/?email=%s&key=%s", domain, u.Email, u.ActivateToken)
-	regMessage := fmt.Sprintf(regMessageRaw, uri)
 	// NOTE: This will block
-	err := smtp.SendMail(addr, auth, from, []string{u.Email}, []byte(regMessage))
+	err := smtp.SendMail(addr, auth, from, []string{email}, []byte(m))
 	if err != nil {
 		log.Printf("Error while sending registration email! (%v)", err)
 	}
 	return err
 }
 
+var sendGRID = func(m, email string) error {
+	sg := sendgrid.NewSendGridClient(username, password)
+	message := sendgrid.NewMail()
+	message.AddTo(email)
+	message.SetSubject("Push registeration")
+	message.SetText(m)
+	message.SetFrom(from)
+	err := sg.Send(message)
+	if err != nil {
+		log.Printf("Error while sending registration email! (%v)", err)
+	}
+	return err
+}
+
+var SendRegistrationEmail = func(u *db.User) error {
+	if !configLoaded {
+		loadConfig()
+	}
+	uri := fmt.Sprintf("%s/activate/?email=%s&key=%s", domain, u.Email, u.ActivateToken)
+	regMessage := fmt.Sprintf(regMessageRaw, uri)
+	return sendMail(regMessage, u.Email)
+}
+
 func loadConfig() {
+	emailType, _ := config.Config.String("email", "type")
+	from, _ = config.Config.String("email", "from")
+	domain, _ = config.Config.String("default", "domain")
 	host, _ = config.Config.String("smtp", "host")
 	port, _ := config.Config.Int("smtp", "port")
-
 	addr = fmt.Sprintf("%s:%d", host, port)
-	domain, _ = config.Config.String("default", "domain")
 
-	username, _ = config.Config.String("smtp", "username")
-	password, _ = config.Config.String("smtp", "password")
-	from, _ = config.Config.String("smtp", "from")
+	switch emailType {
+	case "smtp":
+		username, _ = config.Config.String("smtp", "username")
+		password, _ = config.Config.String("smtp", "password")
+		sendMail = sendSMTP
+		break
+	case "sendgrid":
+		username, _ = config.Config.String("sendgrid", "username")
+		password, _ = config.Config.String("sendgrid", "password")
+		sendMail = sendGRID
+		break
+	default:
+		log.Fatal("Unsupported email type!")
+		break
+	}
 
 	configLoaded = true
 }
